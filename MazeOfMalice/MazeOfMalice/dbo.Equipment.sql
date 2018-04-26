6a775e632cc046a6782a1025eb46e7c2724e52f0
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2018-04-26 13:44:59 +0300

    Database Update 2

---
+++ MazeOfMalice/MazeOfMalice/dbo.Equipment.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Equipment] (
+    [Id]          INT           NOT NULL,
+    [Name]        VARCHAR (50)  NOT NULL,
+    [Description] VARCHAR (MAX) NOT NULL,
+    [Weight]      INT           NOT NULL,
+    [DamageLow]   INT           NULL,
+    [DamageHigh]  INT           NULL,
+    [ArmorBonus]  INT           NULL,
+    [Magical]     REAL          NOT NULL,
+    [Type] VARCHAR(50) NOT NULL, 
+    [SubType] VARCHAR(50) NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
